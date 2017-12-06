165ba840a67b557a193e08a53ef798bd22c2051a
Rmoran95 <rmoran8@uic.edu>
2017-12-06 09:40:40 -0600

    GUI getting built

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
+/*
+    Names: Ryan Moran        ->    rmoran8
+           Edgar Martinez    ->    emart9
+           Faraaz Khan       ->
+
+    Project 5: Messenger Application
+
+    Description: The messenger app will allow for multiple people to join a chat and send messages
+                 between each other depending on who they select in the GUI. The user will be allowed to
+                 choose multiple people and the people they select will only receive the message.
+ */
+
+import javax.swing.*;
+
 public class Main {
 
-    // hello there
+    public static void main(String[] args){
+
+        GUI gui = new GUI();
+        gui.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    }
 }
